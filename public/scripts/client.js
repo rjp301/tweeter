cc9d33820c7343593935acd90b1e363209cbc98b
rjp301 <rileypaul96@gmail.com>
2022-02-02 14:11:19 -0700

    Implement on the fly tweet loading and live tweet adding

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,78 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// const tweetData =  {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// };
+
+$(document).ready(() => {
+  
+
+  $(".new-tweet > form").submit(function (event) {
+    event.preventDefault();
+    const $data = $(this).serialize();
+
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: $data
+    }).then(() => {
+      loadTweets();
+      console.log($data);
+    });
+  });
+
+  
+  const createTweetElement = tweetData => {
+    return $(`
+    <article class="tweet">
+    <header>
+    <div class="tweet-avatar">
+    <img src="${tweetData.user.avatars}" alt="">
+    <div>${tweetData.user.name}</div>
+    </div>
+    <div class="username">${tweetData.user.handle}</div>
+    </header>
+    <p>${tweetData.content.text}</p>
+    <footer>
+    <div>${timeago.format(tweetData.created_at)}</div>
+    <div class="actions">
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
+    </div>
+    </footer>
+    </article>
+    `);
+  };
+  
+  const renderTweets = tweets => {
+    const $tweetContainer = $('section#tweet-container');
+    $tweetContainer.empty();
+    
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $tweetContainer.append($tweet);
+    }
+  };
+  
+  const loadTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+    }).then(data => {
+      console.log(data);
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
+});
--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,18 +1,18 @@
 $(document).ready(() => {
-  $('#tweet-text').on('input', function () {
+  $('#tweet-text').on('input', function() {
     const text = $(this).val();
     const numChars = text.trim().length;
     const numCharsAllowed = 140;
     const numCharsRemain = numCharsAllowed - numChars;
 
     const counter = $(this).siblings().children('.counter');
-    counter.val(numCharsRemain)
+    counter.val(numCharsRemain);
 
     if (numCharsRemain < 0) {
-      counter.addClass('counter-red')
+      counter.addClass('counter-red');
     } else {
-      counter.removeClass('counter-red')
+      counter.removeClass('counter-red');
     }
 
-  })
+  });
 });
